4fe7c1ed1f2da1e27bdb24fce9932eed21ca7a9f
Michael Hertel <mhertel278@gmail.com>
2021-02-20 21:31:09 -0600

    created stations rout

--- app.py
+++ app.py
@@ -44,4 +44,12 @@ def precipitation():
     precip = {date: prcp for date, prcp in precipitation}
     return jsonify(precip)
 
+# create stations route
+@app.route('/api/v1.0/stations')
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
+
+
 
